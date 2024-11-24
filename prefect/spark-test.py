58bda5493e7046e223042ad9f3b2f4e29fa3a3c8
Antoine Buisson <antoine.buisson@sia-partners.com>
2024-11-25 00:43:10 +0100

    trying to package spark app

---
+++ prefect/spark-test.py
@@ -0,0 +1,21 @@
+from pyspark.sql import SparkSession
+import pandas as pd
+
+def process(spark: SparkSession):
+    
+    ddf = spark.createDataFrame(pd.DataFrame({"test": [1, 2, 3]}))
+    
+    ddf_filtered = ddf.filter(ddf.test > 1)
+    
+    return ddf_filtered.toPandas()
+
+if __name__ == "__main__":
+    spark: SparkSession = SparkSession.builder.appName("test").master("local").getOrCreate()
+    
+    spark.sparkContext.setLogLevel("INFO")
+    
+    df = process(spark)
+    
+    print(df)
+    
+    
