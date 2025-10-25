ce5d6fa476278733917935dc2adfe90b213c7fe0
Antoine Buisson <antoine.buisson@sia-partners.com>
2025-10-25 12:40:45 +0200

    adding opentofu folder

---
+++ opentofu/argo-cd.tf
@@ -0,0 +1,19 @@
+resource "kubernetes_namespace" "argo-cd" {
+  metadata {
+    name = "argo-cd"
+  }
+}
+
+resource "helm_release" "argo-cd" {
+  name = "argo-cd"
+
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = "9.0.5"
+
+  namespace = kubernetes_namespace.argo-cd.metadata[0].name
+
+  values = [
+    "${file("files/argo-cd/values.yaml")}"
+  ]
+}
